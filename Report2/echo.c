6127e59c91be7b4ec6ef50cbdcb38c49bd191a82
pwrwpw <113509866+pwrwpw@users.noreply.github.com>
2023-06-12 23:51:27 +0900

    Add files via upload

--- Report2/echo.c
+++ Report2/echo.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc,char *argv[])
-{
+int main(int argc, char *argv[]) {
     int i;
-    for(i=1;i<argc;i++)
-    {
-        printf("%s ",argv[i]);
+    int opt;
+    int n = 0;
+
+    while ((opt = getopt(argc, argv, "n")) != -1) {
+        switch (opt) {
+            case 'n':
+                n = 1;
+                break;
+            default:
+                printf("Usage: %s [-n] [string]\n", argv[0]);
+                return 1;
+        }
     }
-    printf("\n");
+
+    for (i = optind; i < argc; i++) {
+        printf("%s ", argv[i]);
+    }
+
+    if (n == 0) {
+        printf("\n");
+    }
+
     return 0;
 }
